dcee97987f9441b9f0b8d4cc075bf537426bf9fc
EricIkeda1 <93358246+EricIkeda1@users.noreply.github.com>
2024-05-29 15:16:57 -0300

    Add files via upload

---
+++ Login/cadastro_app/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import Produto
+from .models import Fabricante  
+from .models import Venda, VendaItem
+from .models import Feedback
+# Register your models here.
+
+admin.site.register(Produto)
+admin.site.register(Fabricante)
+admin.site.register(Feedback)
+
+@admin.register(Venda)
+class VendaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'data_hora_venda', 'valor_total')
+
+@admin.register(VendaItem)
+class VendaItemAdmin(admin.ModelAdmin):
+    list_display = ('venda', 'produto', 'quantidade', 'valor_total')
