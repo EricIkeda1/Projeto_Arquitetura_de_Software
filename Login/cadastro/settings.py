e591de3333f0528c71bb41f790910cf45c93900a
ikedayuji <ikedayuji.2002@gmail.com>
2024-07-28 19:45:20 -0300

    Deploy 29

--- Login/cadastro/settings.py
+++ Login/cadastro/settings.py
@@ -119,11 +119,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # Diretório estático compartilhado
-    os.path.join(BASE_DIR, 'cadastro_app/static'),  # Diretório específico da aplicação
-]
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
