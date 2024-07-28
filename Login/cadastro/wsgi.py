9054a491e2297b20b3c72df0517b176cf1b701b8
ikedayuji <ikedayuji.2002@gmail.com>
2024-07-28 00:11:28 -0300

    Deploy 25

--- Login/cadastro/wsgi.py
+++ Login/cadastro/wsgi.py
@@ -8,5 +8,5 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 from django.core.wsgi import get_wsgi_application
 
-app = get_wsgi_application()
+application = get_wsgi_application()
 
