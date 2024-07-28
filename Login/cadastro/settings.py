23dff9eca10d00ea3b6e617dd3fd7b8323e4d360
ikedayuji <ikedayuji.2002@gmail.com>
2024-07-28 10:23:19 -0300

    Deploy 31

--- Login/cadastro/settings.py
+++ Login/cadastro/settings.py
@@ -78,6 +78,25 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
+
+
 # Internacionalização
 LANGUAGE_CODE = 'pt-BR'
 TIME_ZONE = 'America/Sao_Paulo'
