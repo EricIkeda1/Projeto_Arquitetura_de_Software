88088222a169a4e9d88978a73f1f1173dfa31c8f
ikedayuji <ikedayuji.2002@gmail.com>
2024-07-28 22:54:07 -0300

    Deploy 46

--- Login/cadastro/settings.py
+++ Login/cadastro/settings.py
@@ -119,10 +119,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# URL para arquivos estáticos
+STATIC_URL = '/static/'
 
+# Diretórios adicionais para procurar arquivos estáticos
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'Login/staticfiles'),  # Diretório para o CSS do Login
+    os.path.join(BASE_DIR, 'cadastro_app/static'), # Diretório para o CSS do Cadastro
+]
+
+# Diretório onde os arquivos estáticos coletados serão armazenados
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
