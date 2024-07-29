670222b854796c7faedd1aa1d47c10ad652544f3
ikedayuji <ikedayuji.2002@gmail.com>
2024-07-29 13:15:44 -0300

    Deploy 63

--- Login/build_files.sh
+++ Login/build_files.sh
@@ -1,2 +1,4 @@
-pip install -r requirements.txt 
-python3.12.3 manage.py collectstatic
+#!/bin/sh
+pip install -r requirements.txt
+python3 manage.py collectstatic --noinput
+python3 manage.py migrate
